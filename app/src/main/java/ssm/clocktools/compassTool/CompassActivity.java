70ae94794b21ed36a7dfc80f5fddfceb35a55011
ssm <songsiming000000@hotmail.com>
2016-05-01 13:24:16 +0800

    v1.0

--- app/src/main/java/ssm/clocktools/compassTool/CompassActivity.java
+++ app/src/main/java/ssm/clocktools/compassTool/CompassActivity.java
@@ -10,7 +10,6 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
